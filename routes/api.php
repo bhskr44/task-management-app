8752e7fb4d47f9af1fab0e3fb50a119f1a61dc5c
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2022-11-27 03:18:42 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/sync-task', [App\Http\Controllers\TaskController::class, 'syncTask']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
